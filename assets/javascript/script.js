7d2c76b0900e3654ee40810118706d88556ef1b7
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2025-04-28 20:28:55 -0300

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,6 +9,11 @@ form.addEventListener("submit", function (e) {
     const imc = (peso / (altura * altura)).toFixed(1);
     // console.log(imc);
 
+    if(peso==0 || altura==0){
+        alert("Valores Inválidos.");
+        return;
+    }
+
     const result = document.getElementById("result");
 
     document.getElementById("infos").classList.remove("hidden");
@@ -19,13 +24,13 @@ form.addEventListener("submit", function (e) {
 
     if (imc < 18.5) {
         description = "Abaixo do peso";
-    } else if (imc >= 18.5 && imc < 24.9) {
+    } else if (imc >= 18.5 && imc <= 24.9) {
         description = "Peso normal";
-    } else if (imc >= 25 && imc < 29.9) {
+    } else if (imc >= 25 && imc <= 29.9) {
         description = "Sobrepeso";
-    } else if (imc >= 30 && imc < 34.9) {
+    } else if (imc >= 30 && imc <= 34.9) {
         description = "Obesidade moderada";
-    } else if (imc >= 35 && imc < 39.9) {
+    } else if (imc >= 35 && imc <= 39.9) {
         description = "Obesidade severa";
     } else if (imc >= 40) {
         description = "Obesidade mórbida";
